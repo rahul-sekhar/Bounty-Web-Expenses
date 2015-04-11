9e73f418383469bd8394aa3c4b6864fc1ef23aac
Rahul Sekhar <sekhar.rahul@gmail.com>
2015-04-11 18:07:22 +0530

    fixed checkbox classes to suit bootstrap

---
+++ app/components/multiselect-checkboxes.js
@@ -0,0 +1,5 @@
+import MultiselectCheckboxesComponent from 'ember-multiselect-checkboxes/components/multiselect-checkboxes';
+
+export default MultiselectCheckboxesComponent.extend({
+  classNames: ['list-inline']
+});
