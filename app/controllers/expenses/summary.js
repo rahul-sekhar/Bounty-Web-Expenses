8c1521dc89e586e9fc9790e324b078618bf8f3ac
Rahul Sekhar <sekhar.rahul@gmail.com>
2015-04-16 17:25:50 +0530

    refactor computation of payments into a service

--- app/controllers/expenses/summary.js
+++ app/controllers/expenses/summary.js
@@ -1,58 +1,8 @@
 import Ember from 'ember';
+import computePayments from 'bounty-web-expenses/services/compute-payments';
 
 export default Ember.Controller.extend({
   paymentsPending: function () {
-    let payments = [];
-
-    let balances = this.get('model.people').map(function (person) {
-      return { person: person, balance: person.get('balance') };
-    });
-
-    // Clear out at least one balance on each iteration
-    for (var i=0; i<balances.length; i++) {
-      // Sort people from the lowest absolute balance amount to highest
-      balances.sort(function (a, b) {
-        return (Math.abs(a.balance) - Math.abs(b.balance));
-      });
-
-      // Skip to the next iteration if this person has no balance to clear
-      let balanceToBeCleared = balances[i].balance;
-      if (!balanceToBeCleared) {
-        continue;
-      }
-
-      // Find the first balance that can be used to clear this
-      for (var j=i+1; j<balances.length; j++) {
-        let balanceToCheck = balances[j].balance;
-
-        // Check whether the balance can be cleared by balanceToCheck
-        // Checking with a delta to account for float inaccuracies
-        if (Math.abs(balanceToCheck + balanceToBeCleared) - Math.abs(balanceToCheck) < 0.001) {
-
-          // Set up the payment
-          if (balanceToCheck > balanceToBeCleared) {
-            payments.push({
-              from: balances[i].person,
-              to: balances[j].person,
-              amount: Math.abs(balanceToBeCleared)
-            });
-          } else {
-            payments.push({
-              from: balances[j].person,
-              to: balances[i].person,
-              amount: Math.abs(balanceToBeCleared)
-            });
-          }
-
-          // Adjust balances
-          balances[i].balance -= balanceToBeCleared;
-          balances[j].balance += balanceToBeCleared;
-
-          break;
-        }
-      }
-    }
-
-    return payments;
+    return computePayments(this.get('model.people'));
   }.property('model.people.@each.balance')
 });
---
+++ app/services/compute-payments.js
@@ -0,0 +1,90 @@
+// This module provides a function that will compute
+// the payments required to settle accounts given an
+// array of people models.
+// An array of payment objects is returned, each
+// containing a 'from' person, 'to' person and an
+// amount
+
+// Class storing each persons temporary balance for computation
+function BalanceObject (person) {
+  this.person = person;
+  this.balance = person.get('balance');
+
+  this.canBeClearedFrom = function(balanceObject) {
+    var balanceAfter = this.balance + balanceObject.balance;
+
+    // Return true if the balance after clearing is lower
+    // in magnitude than the balance before clearing
+    return (Math.abs(balanceAfter) - Math.abs(balanceObject.balance) < 0.001);
+  };
+
+  this.clearFrom = function(balanceObject) {
+    // Adjust balance
+    balanceObject.balance += this.balance;
+
+    // Return a payment object
+    if (this.balance > 0) {
+      return {
+        from: balanceObject.person,
+        to: this.person,
+        amount: Math.abs(this.balance)
+      }
+    } else {
+      return {
+        from: this.person,
+        to: balanceObject.person,
+        amount: Math.abs(this.balance)
+      }
+    }
+  }
+
+  // Check an array of balance objects and
+  // find the first one that can clear this balance object
+  this.clearBalanceFrom = function(balanceObjects) {
+    for (var i=0; i<balanceObjects.length; i++) {
+      var object = balanceObjects[i];
+      if (this.canBeClearedFrom(object)) {
+        return this.clearFrom(object);
+      }
+    }
+  };
+}
+
+// Construction of a balance object from a person
+function getBalanceObject (person) {
+  return new BalanceObject(person);
+}
+
+// Function to sort balance objects by ascending balance amount
+function compareBalanceObjects(a, b) {
+  return (Math.abs(a.balance) - Math.abs(b.balance));
+}
+
+var computePayments = function (peopleArray) {
+  var payments = [];
+  var balanceObjects = peopleArray.map(getBalanceObject);
+
+  // Clear empty balance objects
+  balanceObjects = balanceObjects.filter(function (object) {
+    return object.balance !== 0;
+  });
+
+  // Clear out balance objects until there is just one left
+  while (balanceObjects.length > 1) {
+
+    // Pick the lowest balance
+    balanceObjects.sort(compareBalanceObjects);
+    let currentObject = balanceObjects.shift();
+
+    // Clear the balance from the remaining balance objects
+    let payment = currentObject.clearBalanceFrom(balanceObjects);
+    if (payment) {
+      // Add the returned payment object to the array of payments
+      payments.push(payment);
+    }
+  }
+
+  return payments;
+};
+
+export default computePayments;
